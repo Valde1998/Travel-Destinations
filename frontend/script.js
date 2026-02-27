efba04645e831eb068359780eb2e193c0e0dbde8
Valde1998 <124771370+Valde1998@users.noreply.github.com>
2026-02-27 15:46:03 +0100

    HTML

---
+++ frontend/script.js
@@ -0,0 +1,30 @@
+const apiUrl = "http://127.0.0.1:5000/destinations";
+
+function loadDestinations() {
+    fetch(apiUrl)
+        .then(res => res.json())
+        .then(data => {
+            const container = document.getElementById("destinations");
+            container.innerHTML = "";
+
+            data.forEach(d => {
+                container.innerHTML += `
+                    <div>
+                        <h3>${d.title}</h3>
+                        <p>${d.country}</p>
+                        <button onclick="deleteDestination(${d.id})">Delete</button>
+                    </div>
+                `;
+            });
+        });
+}
+
+function deleteDestination(id) {
+    if (confirm("Are you sure?")) {
+        fetch(`${apiUrl}/${id}`, {
+            method: "DELETE"
+        }).then(() => loadDestinations());
+    }
+}
+
+loadDestinations();
